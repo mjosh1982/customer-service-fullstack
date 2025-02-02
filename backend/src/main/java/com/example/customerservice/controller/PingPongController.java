615d0742383bc5362eaff88b1e2a2bda5fe8d4ea
manojjoshi <manojoshi1982@gmail.com>
2025-02-02 22:47:47 +0530

    updating slack comments. did nothing

--- backend/src/main/java/com/example/customerservice/controller/PingPongController.java
+++ backend/src/main/java/com/example/customerservice/controller/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("pong1");
+        return new PingPong("pong123");
     }
 }
